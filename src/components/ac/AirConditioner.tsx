fee7d4e412d30c39c044fb9ef47a741f4199f80b
LYXOfficial <66897357+LYXOfficial@users.noreply.github.com>
2022-11-25 00:05:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/ac/AirConditioner.tsx
@@ -0,0 +1,168 @@
+import React from 'react'
+import { Fade } from '@mui/material'
+import { EnergyLabel, EnergySavingLabel } from './EnergyLabel'
+import type { AcMode } from '~/types'
+
+import * as pkg from '~/../package.json'
+
+import './AirConditioner.scss'
+import { useAcCtx } from '~/context'
+
+// import { adsenseLink, jumpToAdsense } from "../adsense";
+
+const acColor = {
+  border: '#e0e0e0',
+  display: '#cccccc',
+  wind: '#bbbbbb',
+}
+
+const AcBorder: React.FC<React.PropsWithChildren> = (props) => {
+  return (
+    <div
+      className="bg-white h-38 shadow relative"
+      style={{
+        border: `1px solid ${acColor.border}`,
+        borderRadius: 10,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+      }}
+      {...props}
+    ></div>
+  )
+}
+
+/**
+ * 空调温度
+ * @returns
+ */
+const AcTemperature: React.FC = () => {
+  const { state } = useAcCtx()
+  return (
+    <h4 className="text-4xl text-center">
+      <span className="font-digit ac-temperature">{state.temperature}</span>
+      <small className="font-digit">°C</small>
+    </h4>
+  )
+}
+
+/**
+ * 显示屏（温度/图标）
+ * @param props
+ */
+const AcDisplay = React.forwardRef((props: { mode: AcMode }, ref) => {
+  return (
+    <div
+      className="absolute top-6 right-8"
+      ref={ref as React.RefObject<HTMLDivElement>}
+      style={{
+        color: acColor.display,
+      }}
+    >
+      <h6 className="text-left text-sm">
+        <span>{props.mode === 'cold' ? '❄' : '☀️'}</span>️️
+      </h6>
+      <AcTemperature />
+    </div>
+  )
+})
+
+/**
+ * 空调 Logo
+ * @param props
+ */
+const AcLogo: React.FC = () => {
+  return (
+    <div className="text-center mt-28">
+      <a
+        href={pkg.repository.url}
+        title={pkg.description}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <img
+          className="m-auto"
+          style={{
+            width: 12,
+          }}
+          src="/yun-logo.svg"
+          alt="logo"
+        />
+      </a>
+    </div>
+  )
+}
+
+/**
+ * 出风口线
+ * @returns
+ */
+const AirOutlet: React.FC = () => {
+  return <div className="mt-1" style={{ border: `1px solid ${acColor.border}` }}></div>
+}
+
+/**
+ * 空调状态
+ * @param props
+ */
+const AcStatus: React.FC<{ status: boolean }> = (props) => {
+  // 空调状态小灯
+  const led = { backgroundColor: props.status ? '#38F709' : acColor.border }
+
+  return (
+    <div
+      className="absolute h-1 w-1 rounded-full top-32 right-2"
+      style={{
+        backgroundColor: led.backgroundColor || 'transparent',
+      }}
+    ></div>
+  )
+}
+
+/**
+ * 风特效
+ * @param props
+ */
+const WindEffect = React.forwardRef((props, ref) => {
+  return (
+    <div ref={ref as React.RefObject<HTMLDivElement>} className="wind-effect flex justify-center my-5">
+      <div
+        className="wind-line h-10 w-1"
+        style={{ backgroundColor: acColor.wind, transform: 'rotate(10deg)' }}
+      ></div>
+      <div className="wind-line h-10 w-1 mx-20" style={{ backgroundColor: acColor.wind }}></div>
+      <div
+        className="wind-line h-10 w-1"
+        style={{ backgroundColor: acColor.wind, transform: 'rotate(-10deg)' }}
+      ></div>
+    </div>
+  )
+})
+
+/**
+ * 空调
+ */
+const AirConditioner: React.FC<{
+  mode: AcMode
+  status: boolean
+  temperature: number
+}> = (props) => {
+  return (
+    <div>
+      <AcBorder>
+        <Fade in={props.status}>
+          <AcDisplay mode={props.mode} />
+        </Fade>
+        <AcLogo />
+        <AirOutlet />
+        <AcStatus status={props.status} />
+        <EnergyLabel titleLength={6} />
+        {import.meta.env.VITE_DISABLE_ADSENSE ? null : <EnergySavingLabel />}
+      </AcBorder>
+      <Fade in={props.status} timeout={{ enter: 2500, exit: 1500 }}>
+        <WindEffect />
+      </Fade>
+    </div>
+  )
+}
+
+export default AirConditioner
---
+++ src/components/ac/EnergyLabel.tsx
@@ -0,0 +1,140 @@
+import React from 'react'
+
+interface TextLabelProps {
+  num: number
+  color: string
+  /**
+   * 宽高尺寸
+   */
+  size: number
+  mx: number
+  my: number
+}
+
+/**
+ * 文本标签（黑色小点点）
+ */
+const TextLabel: React.FC<TextLabelProps> = (props) => {
+  const { color, size, mx, my, num } = props
+  const titleLength = [...new Array(num).keys()]
+  const titleLabel = titleLength.map(n => (
+    <span
+      className="text-dot rounded-full"
+      key={n}
+      style={{
+        width: size,
+        height: size,
+        margin: `${my || 0}px ${mx}px`,
+      }}
+    ></span>
+  ))
+  return (
+    <div className="flex justify-center" style={{
+      '--ac-c-text-dot': color,
+    } as any}>
+      {titleLabel}
+    </div>
+  )
+}
+
+/**
+ * 功耗标签
+ */
+export const EnergyLabel: React.FC<{ titleLength: number }> = () => {
+  return (
+    <div
+      className="absolute p-1"
+      style={{
+        backgroundColor: '#4ea5f5',
+        borderRadius: 2,
+        top: 10,
+        left: 10,
+        height: 70,
+        width: 50,
+      }}
+    >
+      <TextLabel num={6} size={4} mx={2} my={0} color="white" />
+      <div
+        className="my-1 h-7 w-full bg-white"
+        style={{
+          padding: '2px 8px',
+        }}
+      >
+        <div className="flex">
+          <div style={{ backgroundColor: 'green', width: '40%' }}></div>
+          <div
+            style={{
+              marginLeft: '40%',
+              height: 3,
+              borderTop: 1.5,
+              borderRight: 2,
+              borderBottom: 1.5,
+              borderLeft: 0,
+              borderTopColor: 'transparent',
+              borderRightColor: 'green',
+              borderBottomColor: 'transparent',
+              borderLeftColor: 'transparent',
+              borderStyle: 'solid',
+            }}
+          ></div>
+          <div style={{ backgroundColor: 'green', width: '10%' }} ></div>
+        </div>
+        <div className="energy-label-level" style={{ backgroundColor: 'lightgreen', width: '50%' }} ></div>
+        <div className="energy-label-level" style={{ backgroundColor: '#ffc107', width: '60%' }} ></div>
+        <div className="energy-label-level" style={{ backgroundColor: 'orange', width: '70%' }} ></div>
+        <div className="energy-label-level" style={{ backgroundColor: 'red', width: '80%' }} ></div>
+      </div>
+      <div
+        className="w-full h-5 bg-white"
+        style={{
+          marginBottom: 2,
+          paddingTop: 0.8,
+        }}
+      >
+        <TextLabel num={11} size={2} mx={0.8} my={2} color="black" />
+        <div className="w-full h-0" style={{ borderTop: '1px solid', margin: '0.8px 0' }}></div>
+        <TextLabel num={9} size={1.5} mx={0.8} my={2} color="black" />
+        <TextLabel num={10} size={1.2} mx={0.8} my={0} color="black" />
+      </div>
+      <TextLabel num={8} size={2} mx={0.8} my={0} color="white" />
+    </div>
+  )
+}
+
+/**
+ * 节能产品惠民工程
+ */
+export function EnergySavingLabel() {
+  return (
+    <a
+      className="adsense-link"
+      href="https://sponsors.yunyoujun.cn"
+      target="_blank" rel="noreferrer"
+    >
+      <div className="energy-saving-label">
+        <div className="energy-saving-label_bg">
+          <span className="energy-saving-label_title">
+            节能产品&nbsp;&nbsp;惠民工程
+          </span>
+          {/* <img
+            className="adsense-logo"
+            src="/images/ximalaya-logo.png"
+            alt="夏日清凉"
+          /> */}
+          <span className="adsense-logo" title="夏日清凉">
+            💰
+          </span>
+          <span className="energy-saving-label_description">
+            推广上限价格：XXXX 元
+          </span>
+          <span className="energy-saving-label_description">
+            政府补助金额：XXXX 元
+          </span>
+          <span className="energy-saving-label_description">
+            补助上限价格：XXXX 元
+          </span>
+        </div>
+      </div>
+    </a>
+  )
+}
